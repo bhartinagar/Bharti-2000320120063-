5d38344f0c9db960675f9074b50b973df3e31884
Bharti <108177859+bhartinagar@users.noreply.github.com>
2023-07-22 11:01:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Loading from './Loading'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'http://20.244.56.144/numbers/primes'
+function App() {
+  return <h2>Number Management Service</h2>
+}
+
+export default App;
---
+++ Loading.js
@@ -0,0 +1,18 @@
+//import React, { useState } from 'react'
+
+/*const Loading = () => {
+
+    const [formValues] = useState([{ name: ""}])
+
+    return (
+        <form  onSubmit={handleSubmit}>
+          {
+          formValues.map((element, index) => (
+            <div className="form-inline" key={index}>
+              <label>Number</label>
+              <input type="text" name="number" value={element.name || ""} onChange={e => handleChange(index, e)} />
+            ))}
+        </form>
+    )
+};
+export default Loading;*/
